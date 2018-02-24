91c22b163001a92e8751a834abda71a6ab9ef91b
Joan Marie Verba <verba001@umn.edu>
2018-02-24 12:18:09 -0600

    noon update

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const name = 'Joan';
+let happy = true;
+let age = 64;
+const hairColor = 'brown';
+
+const joanArray = [name, happy, age, hairColor];
+
+// console.log(typeof name, name);
+// console.log(typeof happy, happy);
+
+// happy = false;
+// console.log(typeof happy, happy);
+// console.log(typeof age, age);
+
+if (joanArray[1] ===  true){
+    console.log(Array.isArray(joanArray), joanArray[3]);
+}
+
+
